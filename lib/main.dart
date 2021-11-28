522dba3235ad5adacf429b20b4ce24c10b3e7eea
rayen-naffouti <naffoutirayen11@gmail.com>
2021-11-28 22:25:43 +0100

    onGenerateRoute

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,39 @@ class ExtractArgumentsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     MaterialApp(
+      // Provide a function to handle named routes.
+      // Use this function to identify the named
+      // route being pushed, and create the correct
+      // Screen.
+      onGenerateRoute: (settings) {
+        // If you push the PassArguments route
+        if (settings.name == PassArgumentsScreen.routeName) {
+          // Cast the arguments to the correct
+          // type: ScreenArguments.
+          final args = settings.arguments as ScreenArguments;
+
+          // Then, extract the required data from
+          // the arguments and pass the data to the
+          // correct screen.
+          return MaterialPageRoute(
+            builder: (context) {
+              return PassArgumentsScreen(
+                title: args.title,
+                message: args.message,
+              );
+            },
+          );
+        }
+        // The code only supports
+        // PassArgumentsScreen.routeName right now.
+        // Other values need to be implemented if we
+        // add them. The assertion here will help remind
+        // us of that higher up in the call stack, since
+        // this assertion would otherwise fire somewhere
+        // in the framework.
+        assert(false, 'Need to implement ${settings.name}');
+        return null;
+      },
       routes: {
         ExtractArgumentsScreen.routeName: (context) =>
             const ExtractArgumentsScreen(),
